55d9889ef0e55611a7d144df160416f477ccfa0b
Rostislav <rostik057@gmail.com>
2024-12-17 00:29:59 +0300

    +add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,12 @@ export default defineNuxtConfig({
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "format-detection", content: "telephone=no" },
       ],
+      link: [
+        {
+          rel: "manifest",
+          href: "/manifest.json",
+        },
+      ],
     },
   },
   pages: true,
@@ -23,13 +29,32 @@ export default defineNuxtConfig({
 
   css: ["~/assets/scss/index.scss"],
 
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
 
   runtimeConfig: {
     public: {
       BASE_URL: process.env.BASE_URL,
       API_URL: process.env.API_URL,
       BACK_URL: process.env.BACK_URL,
+    },
+  },
+
+  pwa: {
+    workbox: {
+      navigateFallback: "/",
+      importScripts: [],
+      runtimeCaching: [
+        {
+          urlPattern: "/",
+          handler: "NetworkFirst",
+          method: "GET",
+        },
+      ],
+    },
+
+    devOptions: {
+      enabled: true,
+      type: "module",
     },
   },
 });
